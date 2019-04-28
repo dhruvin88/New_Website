a43c1540ba6c0a68a271323adf3c14b89cbea1b1
Dhruvin Patel <dhruvinp895@gmail.com>
2019-04-28 10:02:08 -0500

    add file

---
+++ js/change.js
@@ -0,0 +1,33 @@
+function change(page) {
+	var aboutme = document.getElementById('aboutme');
+	var resume = document.getElementById('resume');
+	var contact = document.getElementById('contactme');
+	var projects = document.getElementById('projects');
+
+	switch(page){
+		case 'aboutme':
+			aboutme.style.display = 'block';
+			resume.style.display ='none';
+			contact.style.display = 'none';
+			projects.style.display = 'none';
+			break;
+		case 'projects':
+			aboutme.style.display = 'none';
+			resume.style.display = 'none';
+			contact.style.display = 'none';
+			projects.style.display = 'block';
+			break;
+		case 'resume':
+			aboutme.style.display = 'none';
+			resume.style.display = 'block';
+			contact.style.display = 'none';
+			projects.style.display = 'none';
+			break;
+		case 'contactme':
+			aboutme.style.display = 'none';
+			resume.style.display = 'none';
+			contact.style.display = 'block';
+			projects.style.display = 'none';
+			break;
+	}
+}
